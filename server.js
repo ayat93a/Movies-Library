6c441079d4becfd913688125aded222634c62bc7
ayat93a <alkayedayat93@gmail.com>
2022-02-04 00:08:04 +0200

    edit14

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const express = require ('express');
 const cors = require ( "cors");
 const app = express ();
 const axios = require('axios')
-const pg = require("pg");
-const client= new pg.Client(process.env.databaseUrl)
+const pg = require("pg");//////////task 13
+const client= new pg.Client(process.env.databaseUrl)//////////task 13
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); ///////////task 13
+
 
 app.get('/', movieHandler);
 app.get ('/favorite',favoriteHandler);
@@ -16,9 +17,47 @@ app.get('/trending', trendingHandler);
 app.get('/search', searchHandler);
 app.get('/Networks', networksHandler);
 app.get('/Reviews', reviewshHandler);
-app.get ('*', notFoundHandler);
+app.post("/addMovie", addMovieHandler);////////////////////task13
+app.get("/getMovies", getMoviesHandler)
 
+app.put(`/updateMovie/:id`,updateMoviHandler);///task14
+app.delete(`/deleteMovie/:id`,deleteMovieHandler);///task14
+
+app.get(`/oneMovie/:id` , oneMovieHandler);////task14
+
+app.get ('*', notFoundHandler);
 app.use (errorHandler);
+
+///task14
+function updateMoviHandler(req ,res){
+    //console.log(req.params.id)
+    const id = req.params.id ;
+    const movie = req.body;
+    const sql = `UPDATE mymovies SET title=$1, release_date=$2 , poster_path=$3 , overview = $4 WHERE id=$5 RETURNING *;`
+    let values=[movie.title, movie.release_date, movie.poster_path, movie.overview , id ];
+    client.query(sql,values).then(data=>{
+        res.status(200).json(data.rows);
+    }).catch(error=>{
+        errorHandler(error,req,res)
+    });
+}
+function deleteMovieHandler (req,res){
+    const id = req.params.id;
+    const sql = `DELETE FROM mymovies WHERE id=${id};` 
+    client.query(sql).then(()=>{
+        res.status(200).send("The movie has been deleted");
+    }).catch(error=>{
+        errorHandler(error,req,res)
+    });
+}
+function oneMovieHandler(req,res){
+    let sql = `SELECT * FROM mymovies WHERE id=${req.params.id};`;
+   client.query(sql).then(data=>{
+    res.status(200).json(data.rows);
+ }).catch(err=>{
+     errorHandler(err,req,res)
+ });
+}
 
 ////trending
 function Movies (id, title, release_date , poster_path ,overview){
@@ -81,8 +120,10 @@ let rate = " " ///// my code work perfictly but i didnot found what i shall inpu
 let rewiewUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.api_key}&review_id=${rate}`
 function  reviewshHandler(req,res){
     axios.get(rewiewUrl)
+
     .then(result=>
         {let movies =result.data.results.map(movie=>{
+
         return new Movies(movie.id, movie.title, movie.release_date, movie.poster_path, movie.overview)
     })
     res.status(200).json(movies)
@@ -110,12 +151,34 @@ function favoriteHandler (req,res){
     return res.status(200).send ("Welcome to Favorite Page") ;
 } 
 
-///13
-app.post("/addMovie", addMovieHandler)
+
+//////////////////////////////////////////////////Task13
+
 function addMovieHandler (req,res){
-    //console.log(req.body)
-    res.status(200).send("hi")
+   const movie= req.body
+    let sql = `INSERT INTO mymovies(title,release_date, poster_path, overview) VALUES ($1 , $2 , $3 ,$4)RETURNING *;`
+   let values =[movie.title, movie.release_date, movie.poster_path, movie.overview]
+   client.query(sql,values).then(data =>{
+    res.status(200).json(data.rows);
+   }) .catch (err =>{
+    errorHandler(err,req,res)
+   })
+  
 }
+//////////////
+function getMoviesHandler (req,res){
+    let sql = `SELECT * FROM mymovies;`
+    client.query(sql).then(data=>{
+      res.status(200).json(data.rows);
+    }) .catch (err =>{
+        errorHandler(err,req,res)
+       })
+}
+
+
+//////////////////////errorsHandler
+
+
 ////Handle errors 404
 function notFoundHandler (req,res){
     return res.status(404).send("page not found error")
@@ -126,23 +189,12 @@ function notFoundHandler (req,res){
 function errorHandler (err, req, res) {
     //console.error(error.stack)
     res.status(500).send("Sorry, something went wrong")}
-//     ////////////////////Task 13
 
 
-//app.get ("/getMovies" ,getMoviesHandler)
-
-
-
-
-
-
-
-
-
+////////////////////////listen to port 
 
 client.connect().then (()=>{
    app.listen(4000, ()=>{
        console.log('listening to port 4000')
    })
  })
-
